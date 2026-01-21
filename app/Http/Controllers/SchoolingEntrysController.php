9f8d22b131a9e08989a0647c227cf1b4918d832f
Chrissa Babe Himaya <chrissababehimaya@gmail.com>
2026-01-21 20:45:35 +0800

    School Entry update v3

--- app/Http/Controllers/SchoolingEntrysController.php
+++ app/Http/Controllers/SchoolingEntrysController.php
@@ -19,7 +19,7 @@ class SchoolingEntrysController extends Controller
     public function __construct(SchoolingEntry $schoolingentry)
     {
         $columnHidden = array_merge($schoolingentry->getDates(), ['id']);
-        $columnLabels = ['schoolingunits'  => 'Schooling Unit', 'schooling_unit_id'  => 'Schooling Unit', 'name'  => 'Schooling Unit Name',];    
+        $columnLabels = [];    
         $optionalFields = ['name', 'schooling_unit_id'];
 
         $this->config_data = (object) [
@@ -46,13 +46,18 @@ class SchoolingEntrysController extends Controller
     public function create($id="0")
     {
         abort_if(Gate::denies($this->config_data->module_perm_name.'_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         $schoolingentry = SchoolingEntry::find(1);    
         $schoolingentry->fill([
             'schooling_unit_id' => null,
             'assignment_id' => null,
         ]);
+
         $schoolingunits = SchoolingUnit::all()->pluck('name', 'id');
-        $assignments = Assignment::all()->pluck('name', 'id');
+
+        // Only assignments where type_id = 6
+        $assignments = Assignment::where('type_id', 6)->pluck('name', 'id');
+
         $data_items = [
             "data" => $schoolingentry,
             "column_hidden" => $this->config_data->columnHidden,
@@ -63,6 +68,7 @@ class SchoolingEntrysController extends Controller
             "schoolingunits" => $schoolingunits,
             "assignments" => $assignments,
         ];
+
         return view($this->config_data->module_view_folder.'.show', compact('data_items'));
     }
 
@@ -83,15 +89,16 @@ class SchoolingEntrysController extends Controller
     public function show(SchoolingEntry $schoolingentry)
     {
         abort_if(Gate::denies($this->config_data->module_perm_name.'_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-        $schoolingentry->load('schoolingunits');
-        $columnHidden = array_merge($schoolingentry->getDates(), ['id','schooling_unit_id']);
+        $schoolingentry->load(['schoolingunits', 'assignments']);
+        
+        $columnHidden = array_merge($schoolingentry->getDates(), ['id', 'schooling_unit_id', 'assignment_id']);
         $data_items = [
             "data" => $schoolingentry,
             "column_hidden" => $columnHidden,
             "column_labels" => $this->config_data->columnLabels,
             "operation_type" => "show",
         ];
-
+        // return $sourcedata;
         return view($this->config_data->module_view_folder.'.show', compact('data_items'));
     }
 
@@ -101,7 +108,7 @@ class SchoolingEntrysController extends Controller
     public function edit(SchoolingEntry $schoolingentry)
     {
         abort_if(Gate::denies($this->config_data->module_perm_name.'_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-        $columnHidden = array_merge($schoolingentry->getDates(), ['id']);   
+        $columnHidden = array_merge($schoolingentry->getDates(), ['id']);
         $schoolingunits = SchoolingUnit::all()->pluck('name', 'id');
         $assignments = Assignment::all()->pluck('name', 'id');
         $data_items = [
@@ -186,8 +193,8 @@ class SchoolingEntrysController extends Controller
                       ->get();
 
         // Transform types so DataTables can display them
-        $data = $data->map(function ($schoolingentry) {
-            $schoolingentry->types_list = $schoolingentry->schoolingunits->pluck('name')->toArray();
+        $data = $data->map(callback: function ($schoolingentry) {
+            $schoolingentry->assignments_list = $schoolingentry->assignments->pluck('name')->toArray();
             return $schoolingentry;
         });
 
--- resources/views/references/schoolingentry/show.blade.php
+++ resources/views/references/schoolingentry/show.blade.php
@@ -8,7 +8,7 @@
         @elseif($data_items['operation_type']=="edit")
             <h4 class="d-inline">{{$config_data->module_name}} | EDIT - {{$data_items["data"]->name}}</h4>
         @elseif($data_items['operation_type']=="create")
-            <h4 class="d-inline">{{$config_data->module_name}} | {{$data_items["bulk_insert"]=="bulk" ? "BULK" : ""}} CREATE</h4>            
+            <h4 class="d-inline">{{$config_data->module_name}} | CREATE</h4>            
         @endif
         <a href="{{ route("$config_data->module_route.index") }}" class="btn btn-secondary float-end">
             Back
@@ -35,31 +35,36 @@
                                     {{ $data_items['column_labels'][$key] ?? ucfirst(str_replace('_', ' ', $key)) }}
                                 </th>
                                 <td>
-                                    @if($key=='schooling_unit_id')
-                                        <select name="schooling_unit_id" id="schooling_unit_id" class="form-control select2">
-                                            <option value="">-- Select Schooling Unit --</option>
-                                            @foreach($data_items['schoolingunits'] as $id => $schoolingunit)
-                                                <option value="{{ $id }}"
-                                                    {{ old('schooling_unit_id', $data_items['data']->schooling_unit_id ?? '') == $id ? 'selected' : '' }}>
-                                                    {{ $schoolingunit }}
-                                                </option>
-                                            @endforeach
-                                        </select>
-
-                                    @elseif($key=='assignment_id')
+                                    @if($key=='assignment_id')
                                         <select name="assignment_id" id="assignment_id" class="form-control select2">
                                             <option value="">-- Select Assignment --</option>
                                             @foreach($data_items['assignments'] as $id => $assignment)
                                                 <option value="{{ $id }}"
                                                     {{ old('assignment_id', $data_items['data']->assignment_id ?? '') == $id ? 'selected' : '' }}>
-                                                    {{ $assignment }}
+                                                    {{ $assignment}}
                                                 </option>
                                             @endforeach
                                         </select>
 
                                             @elseif($key=='assignments' && $data_items["operation_type"] === "show")
-                                                <input type="text" class="form-control" value="{{$value['name']}} - {{$data_items['data']->assignments->types->name}}" disabled>                             
-                                        
+                                                <input type="text" class="form-control" value="{{$value['name']}}" disabled>                             
+                                        </td>
+
+                                        <td>
+                                            @elseif($key=='schooling_unit_id')
+                                                <select name="schooling_unit_id" id="schooling_unit_id" class="form-control select2">
+                                                    <option value="">-- Select Schooling Unit --</option>
+                                                    @foreach($data_items['schoolingunits'] as $id => $schoolingunit) 
+                                                        <option value="{{ $id }}"
+                                                            {{ old('schooling_unit_id', $data_items['data']->schooling_unit_id ?? null) == $id ? 'selected' : '' }}>
+                                                            {{ $schoolingunit }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+
+                                            @elseif($key=='schoolingunits' && $data_items["operation_type"] === "show")
+                                                <input type="text" class="form-control" value="{{$value['name']}}" disabled>                             
+                                          
                                             @else
                                                 <input
                                                     type="text"
@@ -79,7 +84,8 @@
                             <td>
                                 <input 
                                     type="submit" 
-                                    class="form-control btn {{ $data_items['operation_type'] === 'create' ? 'btn-primary' : 'btn-warning' }}">
+                                    class="form-control btn {{ $data_items['operation_type'] === 'create' ? 'btn-primary' : 'btn-warning' }}"
+                                >
                             </td>
                         </tr>
                     @endunless
